4d40e8987197fb3114ce3c18ef94930e704088c1
MichiBab <45234706+MichiBab@users.noreply.github.com>
2019-06-19 22:47:28 +0200

    Create Aufgabe10.c

---
+++ Probeklausur/Aufgabe10.c
@@ -0,0 +1,67 @@
+#include <stdint.h>
+#include <stdio.h>int 
+
+
+stringOp(char* s);
+static int isUMLAUT(char c);
+
+uint32_t LEcounter = 0;
+static int isUMLAUT(char c);
+
+
+
+int main(int argc, char **argv){
+  char Quellstring[]="Jubel auf alen Aleen bei alen Leuten, die ales erleben wolen";
+  stringOp(Quellstring);
+	return 0;
+}
+
+
+
+int stringOp(char* s){
+    //stringlänge:
+    int size = 0;
+    char c = s[size];
+    while(c!='\0'){
+        size++;
+        c = s[size];
+        }
+    //länge des ziels errechnen
+    for(int i  = 0; i < size; i++){
+        if(s[i] == 'l'){
+            if(i+1<size && i>0){ //speicherzugriffsschutz
+                if(s[i+1] == 'e'&& isUMLAUT(s[i-1])){
+                    LEcounter++;
+                }
+            }
+        }
+    }
+    //Create Ziel Array:
+    char* Zielstring = malloc(sizeof(char)*(size+LEcounter));
+    if(Zielstring == NULL){
+        return -1;
+        }
+        
+    //zielIndexCounter
+    int zIndex = 0;
+    
+    for(int i = 0; i < size; i++){
+        if(s[i] == 'l'){
+            if(i+1<size+LEcounter && i>0){ //speicherzugriffsschutz
+                if(s[i+1] == 'e'&& isUMLAUT(s[i-1])){
+                    Zielstring[zIndex] = 'l';
+                    zIndex++;
+                    }
+                }
+            }
+        Zielstring[zIndex] = s[i];
+        zIndex++;
+    }
+    printf("%d\n", LEcounter);
+    printf("%s\n", Zielstring);
+    return 0;
+    }
+    
+static int isUMLAUT(char c){
+    return (c =='a' | c =='e' | c =='i' | c =='o' | c =='u' | c =='A' | c =='E' | c =='I' | c =='O' | c =='U') ;
+    }
